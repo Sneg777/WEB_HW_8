f58238d6574f35c948b6d477defabb4bd9f53b75
Yevhenii Bessonov <norent777@gmail.com>
2025-03-31 18:35:30 +0200

    without password

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://Sneg321:wersta159@clusterbes.tzjea.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBes"
+uri = "mongodb+srv://Sneg321:@clusterbes.tzjea.mongodb.net/?retryWrites=true&w=majority&appName=ClusterBes"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["HW_08"]
